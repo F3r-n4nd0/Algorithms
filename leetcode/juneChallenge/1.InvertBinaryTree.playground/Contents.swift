81174cc44217fadb4877a377466d287eb100505c
Fernando Luna <337n4nd0@gmail.com>
2020-06-01 13:14:34 -0400

    Invert Binary Tree

---
+++ leetcode/juneChallenge/1.InvertBinaryTree.playground/Contents.swift
@@ -0,0 +1,57 @@
+import Foundation
+import XCTest
+
+/*
+ Invert Binary Tree
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init() { self.val = 0; self.left = nil; self.right = nil; }
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
+
+extension TreeNode {
+    
+    func inverse() {
+        let lastLeft = self.left
+        self.left = right
+        self.right = lastLeft
+        self.left?.inverse()
+        self.right?.inverse()
+    }
+    
+}
+
+class Solution {
+    
+    func invertTree(_ root: TreeNode?) -> TreeNode? {
+        root?.inverse()
+        return root
+    }
+    
+}
+
