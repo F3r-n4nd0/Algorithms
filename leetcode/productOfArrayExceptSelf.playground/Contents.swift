79a2fb68fe60608bd8363535ccb299923d1b3c0f
Fernando Luna <337n4nd0@gmail.com>
2020-09-22 23:54:29 -0400

    join and update - Product of Array Except Self

--- leetcode/productOfArrayExceptSelf.playground/Contents.swift
+++ leetcode/productOfArrayExceptSelf.playground/Contents.swift
@@ -1,37 +1,66 @@
 import UIKit
 import XCTest
 
-//Given an array nums of n integers where n > 1,  return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+/*
+ Product of Array Except Self
+
+ Given an array nums of n integers where n > 1,  return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+ 
+ */
 
 class Solution {
     func productExceptSelf(_ nums: [Int]) -> [Int] {
-        var prod = 1
-        var result = Array(repeating: 1, count: nums.count)
-        for i in 0..<nums.count{
-            result[i] = prod
+        guard nums.count > 0 else { return [] }
+        var output = Array(repeating: 1, count: nums.count)
+        var prod = nums[0]
+        for i in 1..<nums.count{
+            output[i] = prod
             prod *= nums[i]
         }
-        prod = 1
-        for i in stride(from: nums.count - 1, to: -1, by: -1){
-            result[i] *= prod
+        prod = nums[nums.count-1]
+        for i in stride(from: nums.count - 2, to: -1, by: -1){
+            output[i] *= prod
             prod *= nums[i]
         }
-        return result
+        return output
     }
 }
 
+class Solution2 {
+    func productExceptSelf(_ nums: [Int]) -> [Int] {
+        guard nums.count > 0 else { return [] }
+        var output = Array(repeating: 1, count: nums.count)
+        var sumRight = nums[0] , sumLeft = nums[nums.count-1]
+        output[1] *= sumRight
+        output[nums.count-2] *= sumLeft
+        for i in 2..<nums.count {
+            sumRight *= nums[i-1]
+            output[i] *= sumRight
+            sumLeft *=  nums[nums.count-i]
+            output[nums.count-i-1] *= sumLeft
+        }
+        return output
+    }
+}
 
 class SolutionTests: XCTestCase {
-    var solution: Solution!
-
-    override func setUp() {
-        super.setUp()
-        solution = Solution()
-    }
-
+    
+    var solution = Solution2()
+    
     func testExampleOne() {
         XCTAssertEqual(solution.productExceptSelf([1,2,3,4]), [24,12,8,6])
+    }
+
+    func testExampleTwo() {
         XCTAssertEqual(solution.productExceptSelf([1,0]), [0,1])
+    }
+    
+    func testExampleThree() {
+        XCTAssertEqual(solution.productExceptSelf([4,5,1,8,2]), [80, 64, 320, 40, 160])
+    }
+    
+    func testExampleFour() {
+        XCTAssertEqual(solution.productExceptSelf([9,0,-2]), [0,-18,0])
     }
 }
 
